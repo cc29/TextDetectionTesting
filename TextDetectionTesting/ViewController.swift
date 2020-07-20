ecea693bb08321bc9cd18c509ce449431addb671
chetan <chetan499@icloud.com>
2020-07-20 12:51:35 +0530

    Initial Commit

---
+++ TextDetectionTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextDetectionTesting
+//
+//  Created by Theseus on 20/07/20.
+//  Copyright © 2020 Theseus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
